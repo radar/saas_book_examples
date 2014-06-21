df9e7a015e21acb6a8e7e8488a0cfc966b5b34d3
Ryan Bigg <radarlistener@gmail.com>
2014-06-21 15:15:43 +1000

    Feature #5: Restricting subdomain names

--- subscribem/app/models/subscribem/account.rb
+++ subscribem/app/models/subscribem/account.rb
@@ -3,5 +3,13 @@ module Subscribem
     belongs_to :owner, :class_name => "Subscribem::User"
     accepts_nested_attributes_for :owner
     validates :subdomain, :presence => true, :uniqueness => true
+
+    EXCLUDED_SUBDOMAINS = %w(admin)
+    validates_exclusion_of :subdomain, :in => EXCLUDED_SUBDOMAINS, 
+      :message => "is not allowed. Please choose another subdomain."
+
+    before_validation do
+      self.subdomain = subdomain.to_s.downcase
+    end
   end
 end
